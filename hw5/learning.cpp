d922612f62880397689246c4819913403d11e6ca
Yijia Chen <yijiac@usc.edu>
2019-11-13 15:42:01 -0800

    Nov 13

--- hw5/learning.cpp
+++ hw5/learning.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct course{ // course object
@@ -10,71 +11,39 @@ struct course{ // course object
 	double learn;
 };
 
-// i: number of courses evaluated
+// n: number of courses being evaluated
 // W: work capacity
-// w: current total work
-// l: total learned
-// solutions: all valid solutions
-// build solutions
-void build(vector<course*>& courses, int i, double W, double w, double l, vector<double>& solutions){
-	if (i == (int)courses.size()){
-		solutions.push_back(l);
-	}
-	else{
-		if (w + courses[i]->work <= W){
-			solutions.push_back(l + courses[i]->learn);
-			// 2 recursive calls, either add this course or not
-			build(courses, i+1, W, w+courses[i]->work, l+courses[i]->learn, solutions);
-			build(courses, i+1, W, w, l, solutions);
-		}
-		else{
-			if (courses[i]->work <= W){ // a single course can be a solution
-				solutions.push_back(courses[i]->learn);
-			}
-			build(courses, i+1, W, w, l, solutions);
-		}
-	}
-}
-
-// find the best solution
-double findMax(vector<double>& solutions){
-	double max = 0;
-	for (unsigned int i=0; i<solutions.size(); i++){
-		if (solutions[i] > max){
-			max = solutions[i];
-		}
-	}
-	return max;
+double solver(vector<course*>& courses, int n, double W){
+	if ((n == 0) || (W == 0)) return 0; // base case
+	if (courses[n-1]->work > W) return solver(courses, n-1, W); // skip this course
+	return max(courses[n-1]->learn + solver(courses, n-1, W - courses[n-1]->work), 
+		solver(courses, n-1, W)); // return the maximum of the two
 }
 
 int main(int argc, char* argv[]){
 	ifstream File(argv[1]);
-	if (File.fail()) return -1; // fail to open file
+	if (File.fail()) return 1; // fail to open file
+
 	int n; // total number of classes
 	double W; // work capacity
 	string line; // each line of input
 	vector<course*> courses; // store all courses
-	vector<double> solutions; // store all solutions
 
 	getline(File, line);
 	stringstream s(line); // first line of input
-	s >> n;
-	s >> W;
+	s >> n >> W;
 	for (int i=0; i<n; i++){
 		getline(File, line);
 		stringstream ss(line);
 		course* temp_course = new course; // allocate a new course object
-		ss >> temp_course->name;
-		ss >> temp_course->work;
-		ss >> temp_course->learn;
+		ss >> temp_course->name >> temp_course->work >> temp_course->learn;
 		courses.push_back(temp_course);
 	}
 
-	build(courses, 0, W, 0, 0, solutions); // build solutions piece by piece
-	cout << findMax(solutions) << endl;
+	cout << solver(courses, n, W) << endl; // output the best solution
 
 	for (int i=0; i<n; i++){
-		delete courses[i];
+		delete courses[i]; // delete memory
 	}
 	return 0;
 }
