a8dde3fce6ce22a628a3ac204eae554f50ab1c5a
Yijia Chen <yijiac@usc.edu>
2019-12-08 09:43:02 -0800

    Dec 8

---
+++ hw6/README.md
@@ -0,0 +1,3 @@
+# CSCI 104 HW06   Yijia Chen
+
+- **Problem 1 in file named hw6.pdf.**
--- hw6/bst/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "bst.h"
-#include <string>
-using namespace std;
-
-int main(int argc, char* argv[]){
-	
-	return 0;
-}
